8c9bf92fee4d622b3890ca329694a48bc7b24940
Leandro Cunha <leandrocunha016@gmail.com>
2024-08-04 22:08:18 -0300

    chore: Remove trailing whitespace

--- treemap/treemap.py
+++ treemap/treemap.py
@@ -3,29 +3,30 @@ from tkinter import ttk
 import requests
 import plotly.express as px
 
+
 class CryptoAnalyzer:
     def __init__(self, root):
         # Configuração da janela principal
         self.root = root
         self.root.title("Análise de Criptomoedas")
         self.root.geometry("300x200")
-        
+
         # Criação do frame principal
         self.mainframe = ttk.Frame(root, padding="10 10 10 10")
         self.mainframe.grid(column=0, row=0, sticky=(tk.W, tk.E, tk.N, tk.S))
         self.mainframe.columnconfigure(0, weight=1)
         self.mainframe.rowconfigure(0, weight=1)
-        
+
         # Criação dos widgets da interface
         self.create_widgets()
-    
+
     def create_widgets(self):
         """Cria os widgets da interface gráfica."""
         # Botão para mostrar o Tree Map
         ttk.Button(self.mainframe, text="Mostrar Tree Map", command=self.on_show_treemap).grid(column=1, row=1, sticky=tk.W)
     def fetch_crypto_data(self):
         """Busca os dados das criptomoedas da API CoinGecko.
-        
+
         Returns:
             list: Lista de dicionários contendo dados das criptomoedas.
         """
@@ -43,7 +44,7 @@ class CryptoAnalyzer:
 
     def create_treemap(self, data):
         """Cria e exibe um gráfico do tipo Tree Map usando dados das criptomoedas.
-        
+
         Args:
             data (list): Dados das criptomoedas.
         """
@@ -51,21 +52,21 @@ class CryptoAnalyzer:
         names = [coin['name'] for coin in data]
         market_caps = [coin['market_cap'] for coin in data]
         changes = [coin['price_change_percentage_24h'] for coin in data]
-        
+
         # Criando dicionário de dados para o Plotly
         treemap_data = {
             'name': names,
             'market_cap': market_caps,
             'change': changes
         }
-        
+
         fig = px.treemap(treemap_data, path=['name'], values='market_cap',
                          color='change',
                          color_continuous_scale='RdYlGn',
                          title='Market Cap and 24h Change of Cryptocurrencies')
-        
+
         fig.update_layout(margin=dict(t=50, l=25, r=25, b=25))
-        
+
         fig.show()
 
     def on_show_treemap(self):
