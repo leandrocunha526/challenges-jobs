2547af667cbf9b299d52b4b1445c69420f6b2bbe
Leandro Cunha <leandrocunha016@gmail.com>
2024-08-04 22:28:51 -0300

    chore: Change with PEP 257 and remove unused import
    To comply with PEP 257, which is the style guide for docstrings,
    I can must add a module docstring in addition to docstrings
    for the class and its methods.

--- conversor/conversor.py
+++ conversor/conversor.py
@@ -1,5 +1,25 @@
+"""
+Módulo para conversão de números inteiros para números romanos e vice-versa.
+
+Este módulo contém a classe Conversor que fornece dois métodos:
+- inteiro_para_romano: Converte um número inteiro em um número romano.
+- romano_para_inteiro: Converte um número romano em um número inteiro.
+Módulo em conformidade com PEP 257.
+"""
+
 class Conversor:
+    """Classe para conversão de números inteiros e números romanos."""
+
     def inteiro_para_romano(self, num):
+        """
+        Converte um número inteiro em um número romano.
+
+        Args:
+            num (int): O número inteiro a ser convertido.
+
+        Returns:
+            str: A representação do número romano.
+        """
         val = [
             1000, 900, 500, 400,
             100, 90, 50, 40,
@@ -22,6 +42,15 @@ class Conversor:
         return roman_num
 
     def romano_para_inteiro(self, roman):
+        """
+        Converte um número romano em um número inteiro.
+
+        Args:
+            roman (str): O número romano a ser convertido.
+
+        Returns:
+            int: A representação do número inteiro.
+        """
         roman = roman.upper()
         roman_dict = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
         inteiro = 0
--- conversor/test_conversor_numbers_mock.py
+++ conversor/test_conversor_numbers_mock.py
@@ -1,5 +1,5 @@
 import unittest
-from unittest.mock import MagicMock, patch
+from unittest.mock import patch
 import tkinter as tk
 from conversor_numbers import ConversorNumeros
 from conversor import Conversor
